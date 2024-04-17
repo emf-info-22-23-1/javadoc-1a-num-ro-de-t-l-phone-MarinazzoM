574c0e3bbfc11537d10d66e4f09458ca3f722860
MarinazzoM <MarinazzoM@WSTEMFA44-11.stu.net.fr.ch>
2024-04-17 09:03:44 +0200

    javadoc numéro de téléphone

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -7,7 +7,16 @@ package ch.emf.atelierpo.wrk;
  */
 public class Worker {
 
-
+    /**
+     * 
+     * vérifie le format d'un numéro de téléphone
+     * 
+     * @param prefix le prefix du numéro de téléphone
+     * @param numero le numéro de téléphone
+     * @return true seulement si le préfix est "+4176", "+4177", "+4178" ou "+4179" 
+     * et le numéro comporte 7 chiffres. le format du numéro de téléphone doit être "xxx xx xx" ou "xxxxxxx"
+     */
+    
     public boolean controleNumeroTelMobile(String prefix, String numero) {
         return false;
     }
